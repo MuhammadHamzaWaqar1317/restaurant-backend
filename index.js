a76b4325f1b504a64c83146eb0ce77ccfb6448af
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2024-11-10 15:30:39 +0500

    feat : limit increased of payload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { connectDB } = require("./config/database");
 require("dotenv").config();
 
 connectDB();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
